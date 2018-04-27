b3feebd6019fb5e43fa385fe753a4165cae4115e
Unknown <railken@protonmail.ch>
2018-04-27 14:32:25 +0200

    add concat function

---
+++ src/Languages/BoomTree/Nodes/ConcatFunctionNode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Railken\SQ\Languages\BoomTree\Nodes;
+
+class ConcatFunctionNode extends FunctionNode
+{
+    /**
+     * value.
+     *
+     * @var string
+     */
+    public $value = 'CONCAT';
+}
